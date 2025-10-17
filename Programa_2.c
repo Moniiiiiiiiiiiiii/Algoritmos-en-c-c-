3fb3832aa9e8f300492332bd2d77871506e812d2
Moniiiiiiiiiiiiii <mtcastellanosc@udistrital.edu.co>
2025-10-17 13:46:54 -0500

    Programa_2.c y Programa_3.c

---
+++ Programa_2.c
@@ -0,0 +1,19 @@
+//Programa_2.c
+//Monica Castellanos
+//17 de octubre del 2025
+//asiganción de variables a tipos de datos
+//----------------------------------------------------------------------------
+#include <stdio.h>
+
+int main() {   //Definimos una cantidad entera
+	short enteroNumero1 = 115;  //definimos un numero entero corto
+	signed int enteroNumero2 = -5; //definimos un entero con signo 
+	unsigned long enteroNumero3= 789; 
+	char caracterA = 65; 
+	char caracterB ='B';
+
+	float puntoNumero1 = 89.8; //para decimales
+
+	double  puntoFlotanteNumero2 = 238.952;
+return 0;
+}
